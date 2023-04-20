c08ee13682ba09b27d25121f9e6ef0d0c0ca023a
protapsaha1 <webprotap09@gmail.com>
2023-04-20 13:20:36 +0600

    basic dragon news server

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
+app.get('/', (req, res) => {
+    res.send('Dragon news');
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.listen(port, () => {
+    console.log(`Dragon news is on port ${port}`);
+})
