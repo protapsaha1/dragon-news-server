4dc9cd873a540734d3827ee6cab2d7f81b79f609
protapsaha1 <webprotap09@gmail.com>
2023-04-30 02:48:15 +0600

    vercel deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 
@@ -17,6 +17,23 @@ app.get('/categories', (req, res) => {
 })
 app.get('/news', (req, res) => {
     res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryOfNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryOfNews)
+    }
 })
 
 app.listen(port, () => {
